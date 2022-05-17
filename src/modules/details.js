439ac9d93c15b4178295c548c67e670d2999b368
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-05-17 20:42:14 +0000

    return cocktail after fetch from api

--- src/modules/details.js
+++ src/modules/details.js
@@ -1,6 +1,6 @@
 const displayDetails = async (id) => {
   const cocktail = await fetch(`www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
-  console.log(cocktail);
+  return cocktail;
 };
 
 module.exports = {

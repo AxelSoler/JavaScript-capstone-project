c8b6446afb57a3692f24d178d16b131914c979cd
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-05-18 18:36:15 +0000

    Add instructions section and comment form

--- src/modules/details.js
+++ src/modules/details.js
@@ -1,5 +1,3 @@
-import tempImage from '../img/temp.jpg';
-
 const fetchCocktailById = async (id) => {
   const cocktail = await fetch(
     `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`,
@@ -10,7 +8,10 @@ const fetchCocktailById = async (id) => {
 
 const popup = (details) => {
   console.log(details);
-  const { strDrinkThumb, strDrink } = details;
+  const {
+    strDrinkThumb, strDrink, strInstructions,
+    strIngredient1, strIngredient2, strIngredient3, strIngredient4,
+  } = details;
   const card = document.createElement('div');
   card.className = 'card';
 
@@ -32,17 +33,24 @@ const popup = (details) => {
   infoDiv.className = 'detail-info';
 
   const info1 = document.createElement('p');
-  info1.innerHTML = 'Ingredient: apple';
+  info1.innerHTML = `Ingredient1: ${strIngredient1}`;
 
   const info2 = document.createElement('p');
-  info2.innerHTML = 'Ingredient: apple';
+  info2.innerHTML = `Ingredient2: ${strIngredient2}`;
 
   const info3 = document.createElement('p');
-  info3.innerHTML = 'Ingredient: apple';
+  info3.innerHTML = `Ingredient3: ${strIngredient3}`;
 
   const info4 = document.createElement('p');
-  info4.innerHTML = 'Ingredient: apple';
+  info4.innerHTML = `Ingredient4: ${strIngredient4}`;
 
+  const instructionTitle = document.createElement('h2');
+  instructionTitle.innerHTML = 'Read Instructions';
+  instructionTitle.className = 'instructions-title';
+  const instruction = document.createElement('p');
+  instruction.className = 'instructions';
+
+  instruction.innerHTML = strInstructions;
   const commentDiv = document.createElement('div');
   commentDiv.className = 'comment-div';
   const commentTitle = document.createElement('h2');
@@ -61,21 +69,32 @@ const popup = (details) => {
     card.style.display = 'none';
   });
 
-  const newCommentDiv = document.createElement('div');
-  newCommentDiv.className = 'newCommentDiv';
+  const addCommentDiv = document.createElement('form');
+  addCommentDiv.className = 'new-comment-div';
   const newCommentTitle = document.createElement('h2');
   newCommentTitle.innerHTML = 'Add a comment';
   newCommentTitle.className = 'add-comment-title';
 
-  const input = document.createElement('input');
-  input.className = 'input-comment';
-  const textArea = document.createElement('textarea');
+  const nameInput = document.createElement('input');
+  nameInput.className = 'input-comment';
+  nameInput.placeholder = 'Your name';
 
-  newCommentDiv.append(newCommentTitle, input, textArea);
+  const message = document.createElement('textarea');
+  message.className = 'textarea-input';
+  message.cols = 10;
+  message.rows = 10;
+  message.placeholder = 'Your insights....';
+
+  const button = document.createElement('form');
+  button.textContent = 'COMMENT';
+  button.className = 'submit-button';
+
   commentDiv.append(commentTitle, ul);
   infoDiv.append(info1, info2, info3, info4);
   imageDiv.append(image);
-  card.append(icon, imageDiv, title, infoDiv, commentDiv, newCommentDiv);
+  addCommentDiv.append(newCommentTitle, nameInput, message, button);
+  card.append(icon, imageDiv, title, infoDiv,
+    instructionTitle, instruction, commentDiv, addCommentDiv);
   document.body.appendChild(card);
 };
 

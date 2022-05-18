a60d072bab0627d545b1ff09d8755841d1ef1418
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-05-18 15:46:34 +0000

    Import popup module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import './style.css';
 import logoImg from './img/logo.png';
 import displayItems from './modules/display.js';
+import { popup } from './modules/details';
 
 const logo = document.querySelector('#logoImg');
-
 logo.src = logoImg;
+
+// const commentBtn = document.querySelector('.commentBtn');
+// commentBtn.forEach((element) => {
+//   console.log(element);
+//   element.addEventListener('click', () => {
+//     console.log('hello world');
+//   });
+// });
+popup();
 
 window.onload = () => {
   displayItems();

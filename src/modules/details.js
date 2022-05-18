6a4d257ec8bd58e187538d3f84e42e96fee6e1b0
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-05-18 15:45:18 +0000

    Add popup card to hompage

--- src/modules/details.js
+++ src/modules/details.js
@@ -1,6 +1,34 @@
-const displayDetails = async (id) => {
+import tempImage from '../img/temp.jpg';
+
+const fetchCocktailById = async (id) => {
   const cocktail = await fetch(`www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
   return cocktail;
 };
 
-export default displayDetails;
+const popup = () => {
+  const card = document.createElement('div');
+  card.className = 'card';
+
+  const imageDiv = document.createElement('div');
+  imageDiv.className = 'imageDiv';
+
+  const image = document.createElement('img');
+  image.src = tempImage;
+  image.className = 'detail-image';
+
+  const icon = document.createElement('i');
+  icon.className = 'fa fa-times icon';
+
+  const title = document.createElement('h2');
+  title.innerHTML = 'Margarita';
+  title.className = 'text-title';
+
+  imageDiv.append(image);
+  card.append(icon, imageDiv, title);
+  document.body.appendChild(card);
+};
+
+export {
+  fetchCocktailById,
+  popup,
+};
